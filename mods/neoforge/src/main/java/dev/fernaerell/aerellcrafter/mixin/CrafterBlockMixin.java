6912f165d45342d166492e9d6455c78f897c072a
fernaerell <fernaerell2020@gmail.com>
2025-12-28 13:56:34 +0700

    refactor: Make gradle, mixin code and neoforge.mods.toml for neoforge 1.21.11

--- mods/neoforge/src/main/java/dev/fernaerell/aerellcrafter/mixin/CrafterBlockMixin.java
+++ mods/neoforge/src/main/java/dev/fernaerell/aerellcrafter/mixin/CrafterBlockMixin.java
@@ -7,6 +7,7 @@ import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.CrafterBlock;
 import net.minecraft.world.level.block.entity.CrafterBlockEntity;
 import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.redstone.Orientation;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
@@ -24,8 +25,8 @@ abstract class CrafterBlockMixin {
     }
 
     @Inject(method = "neighborChanged", at = @At("HEAD"), cancellable = true)
-    private void neighborChangedInject(BlockState state, Level level, BlockPos pos, Block neighborBlock, BlockPos neighborPos, boolean movedByPiston, CallbackInfo ci) {
-        if(this.aerellcrafter$isTemplatedOrEmpty(level, pos) && !level.getBlockState(pos).getValue(TRIGGERED)) ci.cancel();
+    private void neighborChangedInject(BlockState blockState, Level level, BlockPos blockPos, Block block, Orientation orientation, boolean moveByPiston, CallbackInfo ci) {
+        if(this.aerellcrafter$isTemplatedOrEmpty(level, blockPos) && !level.getBlockState(blockPos).getValue(TRIGGERED)) ci.cancel();
     }
 
     @Inject(method = "dispenseFrom", at = @At("HEAD"), cancellable = true)
